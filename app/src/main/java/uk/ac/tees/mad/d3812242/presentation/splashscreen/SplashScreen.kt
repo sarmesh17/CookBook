2582ed797f06b3b2cf90b1b47f35e012c0db076c
National <gkrisha66@gmail.com>
2024-12-05 03:45:13 +0545

    Completed splashscreen

---
+++ app/src/main/java/uk/ac/tees/mad/d3812242/presentation/splashscreen/SplashScreen.kt
@@ -0,0 +1,35 @@
+package uk.ac.tees.mad.d3812242.presentation.splashscreen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import uk.ac.tees.mad.d3812242.R
+
+@Composable
+@Preview(showBackground = true)
+fun SplashScreen(){
+Column (modifier = Modifier
+    .fillMaxSize()
+    .background(color = (colorResource(id = R.color.vividOrange)))
+    , horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center){
+
+        Image(painter = painterResource(id = R.drawable.logo), contentDescription =null, modifier = Modifier.size(95.dp,95.dp))
+
+
+
+}
+}
