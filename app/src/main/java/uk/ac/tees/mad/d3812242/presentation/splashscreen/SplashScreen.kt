4b7186bf57f18ba56cccc44e145615c7cadbbc38
National <gkrisha66@gmail.com>
2024-12-05 10:37:44 +0545

    ntg

--- app/src/main/java/uk/ac/tees/mad/d3812242/presentation/splashscreen/SplashScreen.kt
+++ app/src/main/java/uk/ac/tees/mad/d3812242/presentation/splashscreen/SplashScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -15,8 +16,11 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import uk.ac.tees.mad.d3812242.R
 
 @Composable
@@ -28,6 +32,7 @@ Column (modifier = Modifier
     , horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center){
 
         Image(painter = painterResource(id = R.drawable.logo), contentDescription =null, modifier = Modifier.size(95.dp,95.dp))
+    Text(text = "Cook Book", fontSize = 20.sp, fontWeight = FontWeight.Bold, color = Color.White)
 
 
 
